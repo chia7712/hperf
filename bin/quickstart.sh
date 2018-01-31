dea11d9ad9e24c1c634e16faa09c3c20336d0701
Chia-Ping Tsai <chia7712@gmail.com>
2018-01-31 20:29:19 +0800

    Add quickstart script

---
+++ bin/quickstart.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# generate ssh key
+ssh-keygen -t rsa -P '' -f $HOME/.ssh/id_rsa
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+chmod 0600 $HOME/.ssh/authorized_keys
+
+# start ssh service
+/etc/init.d/ssh start
+
+# set Env
+echo "export HADOOP_HOME=$HADOOP_HOME" >> $HOME/.bashrc
+echo "export HBASE_HOME=$HBASE_HOME" >> $HOME/.bashrc
+echo "export JAVA_HOME=$JAVA_HOME" >> $HOME/.bashrc
+
+# list the env variables
+ssh localhost -o StrictHostKeyChecking=no "export"
+
+# deploy hadoop's config
+cp $HPREF_HOME/conf/hadoop/* $HADOOP_HOME/etc/hadoop/
+
+# start hadoop
+$HADOOP_HOME/bin/hdfs dfs namenode --format
+$HADOOP_HOME/sbin/start-dfs.sh
+
+# deploy hbase's config
+cp $HPREF_HOME/conf/hbase/* $HBASE_HOME/conf/
+
+# start hbase
+$HBASE_HOME/bin/start-hbase.sh
